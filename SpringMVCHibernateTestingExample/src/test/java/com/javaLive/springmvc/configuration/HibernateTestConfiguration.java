c79c554a88c9f53eea777aa964f90c18645cb043
Administrator <Administrator@DELL>
2020-02-20 19:32:29 +0530

    Code correction done to enable testing functionality.

--- SpringMVCHibernateTestingExample/src/test/java/com/javaLive/springmvc/configuration/HibernateTestConfiguration.java
+++ SpringMVCHibernateTestingExample/src/test/java/com/javaLive/springmvc/configuration/HibernateTestConfiguration.java
@@ -3,7 +3,7 @@ package com.javaLive.springmvc.configuration;
 import java.util.Properties;
 
 import javax.sql.DataSource;
- 
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,52 +14,62 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
- 
+
 /*
  * This class is same as real HibernateConfiguration class in sources.
  * Only difference is that method dataSource & hibernateProperties 
  * implementations are specific to Hibernate working with H2 database.
  */
- 
+
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({ "com.javaLive.springmvc.dao" })
 public class HibernateTestConfiguration {
- 
-    @Autowired
-    private Environment environment;
- 
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(new String[] { "com.javaLive.springmvc.model" });
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
- 
-    @Bean(name = "dataSource")
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-        return dataSource;
-    }
- 
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
-        return properties;
-    }
- 
-    @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory s) {
-        HibernateTransactionManager txManager = new HibernateTransactionManager();
-        txManager.setSessionFactory(s);
-        return txManager;
-    }
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[] { "com.javaLive.springmvc.model" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+
+	/*
+	 * @Bean(name = "dataSource") public DataSource dataSource() {
+	 * DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	 * dataSource.setDriverClassName(environment.getRequiredProperty(
+	 * "jdbc.driverClassName"));
+	 * dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+	 * dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+	 * dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+	 * return dataSource; }
+	 */
+	@Bean(name = "dataSource")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.hbm2ddl.auto", "create-drop");
+		return properties;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory s) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(s);
+		return txManager;
+	}
 }
